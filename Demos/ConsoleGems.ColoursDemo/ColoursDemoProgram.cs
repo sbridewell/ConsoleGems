db417da718959df82c0418e1c700268f8dc1c1d6
Simon Bridewell <simon@sbridewell.me.uk>
2025-03-06 15:41:41 +0000

    Added ColoursDemo console application project

---
+++ Demos/ConsoleGems.ColoursDemo/ColoursDemoProgram.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="ColoursDemoProgram.cs" company="Simon Bridewell">
+// Copyright (c) Simon Bridewell.
+// Released under the MIT license - see LICENSE.txt in the repository root.
+// </copyright>
+
+namespace ConsoleGems.ColoursDemo
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Sde.ConsoleGems;
+
+    /// <summary>
+    /// Demonstration of how the ConsoleGems library
+    /// uses colours.
+    /// </summary>
+    public static class ColoursDemoProgram
+    {
+        /// <summary>
+        /// Main entry point into the program.
+        /// </summary>
+        /// <param name="args">Command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            System.Console.Title = "ConsoleGems colours demo";
+            var options = new ConsoleGemsOptions { UseColours = true };
+            var services = new ServiceCollection()
+                .AddConsoleGems(options)
+                .AddSingleton<ColourfulConsoleDemo>();
+            var serviceProvider = services.BuildServiceProvider();
+            var demo = serviceProvider.GetRequiredService<ColourfulConsoleDemo>();
+            demo.DemonstrateColours();
+        }
+    }
+}
