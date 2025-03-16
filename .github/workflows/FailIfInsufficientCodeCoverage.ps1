2a876548dc85032c837b2e52bcd0c2945a78c96c
sbridewell <sbridewell@users.noreply.github.com>
2025-03-16 18:06:37 +0000

    Update FailIfInsufficientCodeCoverage.ps1 - write debug output only when run with -Verbose

--- .github/workflows/FailIfInsufficientCodeCoverage.ps1
+++ .github/workflows/FailIfInsufficientCodeCoverage.ps1
@@ -1,10 +1,10 @@
 $coverageFilenames = Get-ChildItem -Path "..\..\ConsoleGems\ConsoleGems\TestResults" -Recurse -Filter "*.xml";
-$coverageFilenames | Format-List -Verbose;
+Write-Verbose ($coverageFilenames | Format-List | Out-String);
 $coverageFilename = $coverageFilenames[0].FullName;
 $coverageFileContent = Get-Content $coverageFilename;
 $coverageXml = [xml]$coverageFileContent;
 $methods = $coverageXml.GetElementsByTagName("Method");
-$methods | Format-List -Verbose -Property name,sequenceCoverage,branchCoverage
+Write-Verbose ($methods | Format-List -Property name,sequenceCoverage,branchCoverage)
 $failures = $methods | Where-Object {[int]$_.sequenceCoverage -lt 80 -or [int]$_.branchCoverage -lt 80};
 if ($failures.Count -gt 0) {
     Write-Output "The following methods have insufficient code coverage";
