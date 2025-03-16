245f6f5ab305592e4e3e61731c8f8835a2813dce
sbridewell <sbridewell@users.noreply.github.com>
2025-03-16 18:25:59 +0000

    Update FailIfInsufficientCodeCoverage.ps1 - set $ErrorActionPreference = 'Stop' in code coverage checking script

--- .github/workflows/FailIfInsufficientCodeCoverage.ps1
+++ .github/workflows/FailIfInsufficientCodeCoverage.ps1
@@ -1,10 +1,11 @@
+$ErrorActionPreference = 'Stop';
 $coverageFilenames = Get-ChildItem -Path "..\..\ConsoleGems\ConsoleGems\TestResults" -Recurse -Filter "*.xml";
 Write-Verbose ($coverageFilenames | Format-List | Out-String);
 $coverageFilename = $coverageFilenames[0].FullName;
 $coverageFileContent = Get-Content $coverageFilename;
 $coverageXml = [xml]$coverageFileContent;
 $methods = $coverageXml.GetElementsByTagName("Method");
-Write-Verbose ($methods | Format-List -Property name,sequenceCoverage,branchCoverage)
+Write-Verbose ($methods | Format-List -Property name,sequenceCoverage,branchCoverage | Out-String);
 $failures = $methods | Where-Object {[int]$_.sequenceCoverage -lt 80 -or [int]$_.branchCoverage -lt 80};
 if ($failures.Count -gt 0) {
     Write-Output "The following methods have insufficient code coverage";
