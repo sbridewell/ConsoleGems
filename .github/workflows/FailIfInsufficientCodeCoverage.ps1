abafbb16a7410f444ac26d73cfc36349dc872c3a
Simon Bridewell <simon@sbridewell.me.uk>
2025-03-16 15:29:06 +0000

    PowerShell script to fail workflow if code coverage is less than 80%

---
+++ .github/workflows/FailIfInsufficientCodeCoverage.ps1
@@ -0,0 +1,10 @@
+﻿$coverageFileContent = [xml](Get-Content ..\..\ConsoleGems.Test\coverage.opencover.xml);
+$methods = $coverageFileContent.GetElementsByTagName("Method");
+$failures = $methods | Where-Object {[int]$_.sequenceCoverage -lt 80 -or [int]$_.branchCoverage -lt 80};
+if ($failures.Count -gt 0) {
+    Write-Output "The following methods have insufficient code coverage";
+    $failures | ForEach-Object {
+        $_ | Select-Object -Property name,sequenceCoverage,branchCoverage
+    } | Format-List
+    throw "Insufficient code coverage";
+}
