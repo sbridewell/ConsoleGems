893a8d20d863483baedfbd193bc02a2f2c5e3146
Simon Bridewell <simon@sbridewell.me.uk>
2025-03-17 20:13:08 +0000

    Tidy up FailIfInsufficientCodeCoverage.ps1 now that it seems to be working

--- .github/workflows/FailIfInsufficientCodeCoverage.ps1
+++ .github/workflows/FailIfInsufficientCodeCoverage.ps1
@@ -7,11 +7,12 @@ $coverageFileContent = Get-Content $coverageFilename;
 $coverageXml = [xml]$coverageFileContent;
 $methods = $coverageXml.GetElementsByTagName("Method");
 $methodCount = ($methods | Measure-Object).Count;
-Write-Output "Found $methodCount methods";
-Write-Output ($methods | Format-List -Property name,sequenceCoverage,branchCoverage | Out-String);
+Write-Verbose "Found $methodCount methods";
+Write-Verbose ($methods | Format-List -Property name,sequenceCoverage,branchCoverage | Out-String);
+
+# Explicitly wrap the result in an array, otherwise if there's only one object found then it's returned
+# as a XmlElement rather than as an array with a single element.
 $failures = @($methods | Where-Object {[int]$_.sequenceCoverage -lt 80 -or [int]$_.branchCoverage -lt 80});
-$failures | Get-Member;
-$failures;
 if ($failures.Count -gt 0) {
     Write-Output "The following methods have insufficient code coverage";
     $failures | ForEach-Object {
