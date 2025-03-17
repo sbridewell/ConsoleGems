9f05d89eec70874473ed3223769185422fe04aeb
Simon Bridewell <simon@sbridewell.me.uk>
2025-03-17 19:53:47 +0000

    FailIfInsufficientCodeCoverage.ps1 - display members and value of failing test method(s)

--- .github/workflows/FailIfInsufficientCodeCoverage.ps1
+++ .github/workflows/FailIfInsufficientCodeCoverage.ps1
@@ -1,6 +1,6 @@
 Write-Verbose "You are running in verbose mode";
 $ErrorActionPreference = 'Stop';
-$coverageFilenames = Get-ChildItem -Path "..\..\ConsoleGems\ConsoleGems\TestResults" -Recurse -Filter "*.xml";
+$coverageFilenames = Get-ChildItem -Recurse -Filter "coverage.opencover.xml";
 Write-Verbose ($coverageFilenames | Format-List | Out-String);
 $coverageFilename = $coverageFilenames[0].FullName;
 $coverageFileContent = Get-Content $coverageFilename;
@@ -9,7 +9,9 @@ $methods = $coverageXml.GetElementsByTagName("Method");
 $methodCount = ($methods | Measure-Object).Count;
 Write-Output "Found $methodCount methods";
 Write-Output ($methods | Format-List -Property name,sequenceCoverage,branchCoverage | Out-String);
-$failures = $methods | Where-Object {<#[int]$_.sequenceCoverage -lt 80 -or#> [int]$_.branchCoverage -lt 80};
+$failures = $methods | Where-Object {[int]$_.sequenceCoverage -lt 80 -or [int]$_.branchCoverage -lt 80};
+$failures | Get-Member;
+$failures;
 if ($failures.Count -gt 0) {
     Write-Output "The following methods have insufficient code coverage";
     $failures | ForEach-Object {
